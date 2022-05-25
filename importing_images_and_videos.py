ba6c813fdfcebe497d829beeda97cfe781c819a8
Tejash-111 <93814016+Tejash-111@users.noreply.github.com>
2022-05-25 14:44:31 +0530

    Create importing_images_and_videos.py

---
+++ importing_images_and_videos.py
@@ -0,0 +1,18 @@
+import cv2
+
+# to output an image to screen.
+img = cv2.imread("resources/test.png")
+cv2.imshow("Output", img)
+cv2.waitKey(1000)
+
+# to output a video to screem
+cap = cv2.VideoCapture("resources/test1.mp4")
+# to display video as a set of images.
+while True:
+    success, img2 = cap.read()
+    cv2.imshow
+    # exit the video when "q" is pressed.
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
